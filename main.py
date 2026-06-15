db253ed752eefdce285105e9434e03d8928ee2e0
EugeneSdrv <eugene.p.sudarev@gmail.com>
2026-06-15 10:56:25 +0300

    feat: initial working version of script

---
+++ main.py
@@ -0,0 +1,117 @@
+import csv
+import json
+
+import httpx
+from httpx import Response
+from pydantic import BaseModel, ValidationError
+
+token = "e41ee64c1c4e326d207e0362ae20bb46d76bb969"
+secret = "81cac1d4980ab5242094483cf15a7c6381891006"
+email = ["serega@yandex/ru"]
+
+
+class ResponseIn(BaseModel):
+    source: str
+    email: str
+    local: str
+    domain: str
+    type: str
+    qc: int
+
+
+class ToCSVAdapter:
+    # TODO: валидировать что входящий код в рамках данного словаря
+    verification_description_by_code = {
+        0: "Корректное значение соответствует общепринятым правилам",
+        2: "Пустое или заведомо «мусорное» значение",
+        3: "«Одноразовый» адрес. Домены 10minutemail.com, getairmail.com, temp-mail.ru и аналогичные",
+        1: "Некорректное значение. Не соответствует общепринятым правилам",
+        4: "Исправлены опечатки",
+    }
+    # TODO: валидировать что входящий код в рамках данного словаря
+    email_type = {
+        "PERSONAL": "личный",  # (@mail.ru, @yandex.ru)
+        "CORPORATE": "корпоративный",  # (@myshop.ru)
+        "ROLE": "ролевой",  # (info@, support@)
+        "DISPOSABLE": "одноразовый",  # (@temp-mail.ru)
+    }
+
+    def __init__(self, response_data):
+        self.email = response_data.email
+        self.domain = response_data.domain
+        if self._validate_field(field=response_data.type, cls_field_name="email_type"):
+            setattr(self, "email_type", self.email_type[response_data.type])
+        if self._validate_field(field=response_data.qc, cls_field_name="verification_description_by_code"):
+            setattr(self, "verification_description", self.verification_description_by_code[response_data.qc])
+
+    # TODO: валидировать что входящий код в рамках данного словаря
+    def _validate_field(self, field, cls_field_name: str):
+        if field not in getattr(self, cls_field_name):
+            # TODO: динамически генерировать исключение связанное с ошибкой валидации конкретного поля
+            raise
+        else:
+            return True
+
+
+def save_email_info_to_csv(email_info_data: ResponseIn):\
+    # TODO: преобразование должно быть вне функции сохранения
+    adapter = ToCSVAdapter(email_info_data)
+    data = vars(adapter)
+    with open('email.csv', mode='w', encoding='utf-8', newline='') as file:
+        fieldnames = data.keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        # TODO: сохранять в csv нужно список словарей а не один объект!!!
+        writer.writerows([data])
+
+
+def save_response(response: Response) -> None:
+    with open('response.json', 'w', encoding='utf-8') as file:
+        json.dump(response.json(), file)
+
+
+def sent_request(email_in: str) -> Response:
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Token {token}",
+        "X-Secret": secret,
+    }
+    # TODO: обработать ошибки недоступности API, возможно добавить ретраи
+    try:
+        return httpx.post(
+            url='https://cleaner.dadata.ru/api/v1/clean/email',
+            headers=headers,
+            json=email_in,
+        )
+    except:
+        raise
+
+def load_response():
+    with open('response.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def main():
+    raw_email_info_data = load_response()
+    # raw_email_info_data = sent_response(email)
+    # TODO: обрабатывать ошибки с неверным форматом входящих данных
+    try:
+        for raw_email_info in raw_email_info_data:
+            # TODO: возможно стоит выделить валидацию и обработку ошибок в отдельную функцию как в отправке запроса
+            email_info_data : ResponseIn = ResponseIn.model_validate(raw_email_info)
+            # TODO: сохранять в csv нужно список словарей а не один объект!!!
+            save_email_info_to_csv(email_info_data)
+            return email_info_data
+    except ValidationError:
+        # TODO: сохранить лог о том что не прошла обработка валидации, поэтому ... (либо - ретраи, либо - просто
+        #  продолжать и сохранять почту, как необработанную)
+        # raise  # не нужно райзить, а наоборот обрабатывать ошибку и продолжать выполнение программы
+        # возможно можно райзануть HttpException с 4хх кодом ,например, данную строку не удалось обработать.
+        # И сохранять логи, что данные по ней приходят невалидные (по структуре, что вряд ли может быть)
+        pass
+
+
+# TODO: добавить обработку нескольких email адресов в соответствии с ограничениями API
+if __name__ == "__main__":
+    main()
